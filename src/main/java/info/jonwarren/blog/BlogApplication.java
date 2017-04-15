4a2d0e35173d1dda8edacdafda7d06f59b52b788
Jon Warren <jon@jonwarren.info>
2017-04-15 15:48:50 -0500

    Roughed in, as comments, some potentially needed code

--- src/main/java/info/jonwarren/blog/BlogApplication.java
+++ src/main/java/info/jonwarren/blog/BlogApplication.java
@@ -6,7 +6,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
-	}
+//    private static final Logger logger = Logger.getLogger(BlogApplication.class);
+
+//    @Autowired 
+//    PostRepository postRepository;
+    
+//    @Autowired
+//    DataLoader dataLoader;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+//    @PostConstruct
+//    void seePosts(){
+//        for(Post post : postRepository.findAll() ){
+//            logger.info(post.toString());
+//        }
+//    }
+
 }
